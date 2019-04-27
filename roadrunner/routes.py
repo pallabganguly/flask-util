f075b8b0a3ec6f23184ef0679c77314a093aaeba
pallabganguly <pallabkumarganguly@gmail.com>
2019-04-27 23:38:05 +0530

    develop

--- roadrunner/routes.py
+++ roadrunner/routes.py
@@ -9,6 +9,7 @@ def index():
     print("Logger: Index Page")
     form = ScriptForm(request.form)
     print(form.validate(), request.method)
+    os.system('python3 scriptfolder/script.py')
     if form.validate() and request.method == 'POST':
         print("Logger:", form.scriptname.data, form.arguments.data)
         os.system('python3 scriptfolder/'+form.scriptname.data)
